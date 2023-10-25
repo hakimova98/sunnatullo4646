817168833cfd6c7d569471ce120502db65d9f697
hakimova98 <148937681+hakimova98@users.noreply.github.com>
2023-10-25 13:06:42 +0500

    Create if5.py

---
+++ if5.py
@@ -0,0 +1,18 @@
+a=int(input('a'))
+b=int(input('b'))
+c=int(input('c'))
+x=0 # +
+y=0 # -
+if a>0:
+    x=x+1
+else:
+    y=y+1
+if b>0:
+    x=x+1
+else:
+    y=y+1
+if c>0:
+    x=x+1
+else:
+    y=y+1
+print('+chisla',x,'-chisla',y)
